49e157cd2ee63295f1f19f073a3cb08a339395a2
samjerguson <50676810+samjerguson@users.noreply.github.com>
2022-10-30 19:32:25 -0500

    Added enemy raycast

--- Lidar Game/Assets/Scripts/RayCast.cs
+++ Lidar Game/Assets/Scripts/RayCast.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class RayCast : MonoBehaviour
 {
 
-	public LayerMask mask;
-	public Object prefab;
+	public LayerMask NotEnemy;
+	public LayerMask Enemy;
+	public Object prefabWhite;
+	public Object prefabRed;
 	public float Spread;
 	public float ShootDistance;
 
@@ -18,14 +20,19 @@ public class RayCast : MonoBehaviour
 		RaycastHit hitInfo;
 		if (Input.GetKey(KeyCode.Mouse0))
 		{
-			if (Physics.Raycast(ray, out hitInfo, ShootDistance))
+			if (Physics.Raycast(ray, out hitInfo, ShootDistance, NotEnemy))
 			{
 				Debug.DrawLine(ray.origin, hitInfo.point, Color.red);
-				Object.Instantiate(prefab, hitInfo.point, Quaternion.identity);
+				Object.Instantiate(prefabWhite, hitInfo.point, Quaternion.identity);
 			}
 			else
 			{
 				Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100, Color.green);
+			}
+			if (Physics.Raycast(ray, out hitInfo, ShootDistance, Enemy))
+			{
+				Debug.DrawLine(ray.origin, hitInfo.point, Color.red);
+				Object.Instantiate(prefabRed, hitInfo.point, Quaternion.identity);
 			}
 		}
 
