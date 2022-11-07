b2c27f53f93a0a6e5c0ae4dd9fe4ef638789e942
samjerguson <50676810+samjerguson@users.noreply.github.com>
2022-11-06 19:00:27 -0600

    UI Implementation
    Implemented the UI along with 3 different spread modes for raycasting

--- Lidar Game/Assets/Scripts/RayCast.cs
+++ Lidar Game/Assets/Scripts/RayCast.cs
@@ -10,11 +10,20 @@ public class RayCast : MonoBehaviour
 	public Object prefabWhite;
 	public Object prefabRed;
 	public float Spread;
+	public float lowSpread, medSpread, highSpread;
+	public float lowDistance, medDistance, highDistance;
 	public float ShootDistance;
+	int qPresses = 0;
 
+	void Start()
+	{
+		Spread = medSpread;
+		ShootDistance = medDistance;
+	}
 
 	void Update()
 	{
+		CheckModeSwitch();
 
 		Ray ray = new Ray(transform.position, new Vector3(Random.Range(transform.up.x - Spread, transform.up.x + Spread), Random.Range(transform.up.y - Spread, transform.up.y + Spread), Random.Range(transform.up.z - Spread, transform.up.z + Spread)));
 		RaycastHit hitInfo;
@@ -36,5 +45,38 @@ public class RayCast : MonoBehaviour
 			}
 		}
 
+	}
+
+	void CheckModeSwitch()
+	{
+		if (Input.GetKeyDown(KeyCode.Q))
+		{
+			int qFinal = qPresses % 3;
+			qPresses++;
+			print("Spread Mode: " + qFinal);
+			switch(qFinal)
+			{
+				case 0:
+				{
+					Spread = lowSpread;
+					ShootDistance = highDistance;
+					break;
+				}
+				case 1:
+				{
+					Spread = medSpread;
+					ShootDistance = medDistance;
+					break;
+				}
+				case 2:
+				{
+					Spread = highSpread;
+					ShootDistance = lowDistance;
+					break;
+				}
+				default:
+					break;
+			}
+		}
 	}
 }
---
+++ Lidar Game/Assets/Scripts/UI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+
+    public Text spreadText;
+    public GameObject gun;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckSpread();
+    }
+
+    void CheckSpread()
+    {
+        if(gun.GetComponent<RayCast>().highSpread == gun.GetComponent<RayCast>().Spread)
+            spreadText.text = "Current Spread: Wide";
+        else if(gun.GetComponent<RayCast>().medSpread == gun.GetComponent<RayCast>().Spread)
+            spreadText.text = "Current Spread: Medium";
+        else if(gun.GetComponent<RayCast>().lowSpread == gun.GetComponent<RayCast>().Spread)
+            spreadText.text = "Current Spread: Narrow";
+        else
+            spreadText.text = "Current Spread: Unknown";
+    }
+}
