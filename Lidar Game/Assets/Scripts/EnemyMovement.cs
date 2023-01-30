05a7e2dc0d8903746387111932d2d8c98081433e
samjerguson <50676810+samjerguson@users.noreply.github.com>
2023-01-29 20:45:37 -0600

    AI pathfinding and Enemy movement

---
+++ Lidar Game/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform player;
+    public float speed = 5f;
+    public NavMeshAgent agent;
+
+    void Update()
+    {
+        //Vector3 direction = player.position - transform.position;
+        //transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        //transform.rotation = Quaternion.LookRotation(direction);
+        agent.SetDestination(player.position);
+    }
+}
