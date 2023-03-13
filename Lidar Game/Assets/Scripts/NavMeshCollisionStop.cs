4d1ac8b187453755f8780fc65dab7aea9014b40c
samjerguson <50676810+samjerguson@users.noreply.github.com>
2023-03-12 22:12:24 -0500

    Enemy collision avoidance

---
+++ Lidar Game/Assets/Scripts/NavMeshCollisionStop.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class AvoidNavMeshOverlap : MonoBehaviour
+{
+    private NavMeshAgent navAgent;
+
+    void Start()
+    {
+        // Get the NavMeshAgent component from this game object
+        navAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        // Check if the NavMeshAgent is currently moving
+        if (navAgent.velocity != Vector3.zero)
+        {
+            // Get all NavMeshAgents within a certain distance from this object
+            Collider[] colliders = Physics.OverlapSphere(transform.position, 2f);
+
+            // Loop through all the colliders
+            foreach (Collider collider in colliders)
+            {
+                // Check if the collider is a NavMeshAgent
+                if (collider.GetComponent<NavMeshAgent>())
+                {
+                    // Calculate the distance between this object and the collider
+                    float distance = Vector3.Distance(transform.position, collider.transform.position);
+
+                    // If the distance is less than the sum of the radii of the two NavMeshAgents, stop this NavMeshAgent
+                    if (distance < navAgent.radius + collider.GetComponent<NavMeshAgent>().radius)
+                    {
+                        navAgent.isStopped = true;
+                        break;
+                    }
+                    else
+                    {
+                        navAgent.isStopped = false;
+                    }
+                }
+            }
+        }
+    }
+}
