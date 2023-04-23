0dfbe1ca2e18fbaea3bc3626252d98ec0ce74a68
Rishab Bhat <62089024+RishabBhat@users.noreply.github.com>
2023-04-23 15:31:33 -0500

    Update Startup.cs

--- Lidar Game/Assets/Scripts/Startup.cs
+++ Lidar Game/Assets/Scripts/Startup.cs
@@ -4,6 +4,15 @@ Sam Jerguson
 Created Feb 27, 2023
 Revised Feb 27, 2023
 This file controls the unity engine bootup, and begins the music as well as the initiial start of the game along with an update function that is called.
+Preconditions: 
+- none
+Postconditions: 
+- The game's background music starts playing and the game is ready to be played.
+Error/exception conditions: 
+- If the audio clip is not found or cannot be played, an error message will be displayed.
+Side Effects: 
+-  None
+No known Faults
 No known Faults
 */
 using System.Collections;
