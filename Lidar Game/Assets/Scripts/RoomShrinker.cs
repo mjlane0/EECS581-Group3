f267a93b2bf42fdb6c6e6ddddf7d7b344b02f300
Rishab Bhat <62089024+RishabBhat@users.noreply.github.com>
2023-04-23 15:27:58 -0500

    Update RoomShrinker.cs

--- Lidar Game/Assets/Scripts/RoomShrinker.cs
+++ Lidar Game/Assets/Scripts/RoomShrinker.cs
@@ -4,6 +4,17 @@ Sam Jerguson
 Created April 8, 2022
 Revised April 8, 2022
 This file controls the walls in the enviornment of the game, through the 4 wall variables and an update function it is able to calculate and move the position of the walls.
+Preconditions: 
+
+- The script is attached to a GameObject in the game.
+- The four wall game objects (wall1, wall2, wall3, wall4) are assigned in the Inspector of the script's GameObject.
+Postconditions: 
+- The positions of the four walls will move towards the center of the room at a constant rate, specified by the shrinkSpeed variable.
+Error/exception conditions: 
+- If the script is not attached to a GameObject in the game, an error will occur.
+- If one or more of the wall game objects (wall1, wall2, wall3, wall4) are not assigned in the Inspector of the script's GameObject, an error will occur.
+Side Effects: 
+- none
 No known Faults
 */
 
